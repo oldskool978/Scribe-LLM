aa5c27649595c81e8f4b33340833d972b4312224
David <48716362+oldskool978@users.noreply.github.com>
2026-09-01 20:46:10 -0400

    Refactor ScribePhysicsEngine class implementation

--- static/js/physics_engine.js
+++ static/js/physics_engine.js
@@ -1,5 +1,3 @@
-// Copyright (c) 2026 Hector Nunez. Licensed under PolyForm Shield License 1.0.0. See LICENSE.md.
-
 class ScribePhysicsEngine {
     constructor() {
         this.CONTEXT_BOUNDS = [512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288, 1048576];
